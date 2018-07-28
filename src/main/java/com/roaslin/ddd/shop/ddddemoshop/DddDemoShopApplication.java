c534ae604749c0f79841e992fe0f032d349be407
roaslin <roasiln@gmail.com>
2018-07-28 22:46:32 +0200

    - first commit

---
+++ src/main/java/com/roaslin/ddd/shop/ddddemoshop/DddDemoShopApplication.java
@@ -0,0 +1,12 @@
+package com.roaslin.ddd.shop.ddddemoshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DddDemoShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DddDemoShopApplication.class, args);
+	}
+}
